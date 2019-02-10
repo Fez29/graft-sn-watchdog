866c357de2a1d17e804fdfd0a362f55fa561f979
gareth <gareth>
2019-02-10 22:43:31 +0200

    reverted changes

--- all_scripts.sh
+++
@@ -1,6 +0,0 @@
-for i in "/home/graft-sn/supernode/graft-sn-watchdog/scripts/"*
-do
-    "$i" &
-done
-
-wait
--- python.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-bash -c 'sleep 350 && python3 snwatchdog.py --log-level 1 > watchdog.log 2>&1 &'
